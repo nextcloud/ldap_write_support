54d319e9d58d15e00c73a0930cc8cbc0b4a61222
Alan <alantygel@gmail.com>
2017-01-14 10:46:07 -0300

    added hooks function / start ldap connection

--- lib/Hooks.php
+++ lib/Hooks.php
@@ -4,19 +4,10 @@ namespace OCA\LdapUserManagement;
 
 class Hooks {
 
-	/**
-	 * clean up user specific settings if user gets deleted
-	 * @param array $params array with uid
-	 *
-	 * This function is connected to the pre_deleteUser signal of OC_Users
-	 * to remove the used space for the trash bin stored in the database
-	 */
 	public static function createLDAPUser_hook($params) {
 		if( \OCP\App::isEnabled('ldapusermanagement') ) {
 			// create LDAP User
-			$fid = fopen('/var/www/html/server/apps/ldapusermanagement/log.txt', 'w');
-			fwrite($fid, "createLDAPUser: " . $params['uid'] . " >> " . $params['password'] . "\n");
-			fclose($fid);
+			\OCA\LdapUserManagement\LdapUserManagement::createLDAPUser($params);
 		}
 	}
 
@@ -31,10 +22,8 @@ class Hooks {
 
 	public static function deleteLDAPUser_hook($params) {
 		if( \OCP\App::isEnabled('ldapusermanagement') ) {
-			// remove NextCloud User
-			$fid = fopen('/var/www/html/server/apps/ldapusermanagement/log.txt', 'a');
-			fwrite($fid, "deleteLDAP: " . $params['uid'] . "\n");
-			fclose($fid);
+			// remove LDAP User
+			\OCA\LdapUserManagement\LdapUserManagement::deleteLDAPUser($params);
 		}
 	}
 
--- lib/LdapUserManagement.php
+++ lib/LdapUserManagement.php
@@ -15,4 +15,83 @@ class LdapUserManagement {
 		\OCP\Util::connectHook('OC_User', 'pre_deleteUser', 'OCA\LdapUserManagement\Hooks', 'deleteLDAPUser_hook');
 
 	}
+
+	public static function createLDAPUser($params) {
+	/**
+	 * create LDAP user
+	 */
+		$ds = LdapUserManagement::connectLDAP();
+		if (LdapUserManagement::disconnectLDAP($ds)) {
+			$result = "OK";
+		} else {
+			$result = "Not OK";
+		}
+		$fid = fopen('/var/www/html/server/apps/ldapusermanagement/log.txt', 'w');
+		fwrite($fid, "createLDAPUser: " . $params['uid'] . " >> " . $params['password'] . " >> $result \n");
+		fclose($fid);
+	}
+
+	public static function deleteLDAPUser($params) {
+	/**
+	 * create LDAP user
+	 */
+		$uid = $params['uid']; // não é esso o parametro!!!
+		$ds = LdapUserManagement::connectLDAP();
+
+
+		$dn = "dc=localhost";
+		$filter="(&(|(objectclass=inetOrgPerson)(objectclass=posixAccount)(objectclass=top))(|(|(mailPrimaryAddress=$uid)(mail=$uid))(|(dc=$uid)(o=$uid)(objectClass=$uid))))";
+		$justthese = array("ou", "sn", "givenname", "mail");
+    	$sr=ldap_search($ds, $dn, $filter, $justthese);
+		$info = ldap_get_entries($ds, $sr);
+
+		if ($info['count'] == 1) {
+			$result = "User Exist";
+		} else {
+			$result = "User not found";
+		}
+
+		LdapUserManagement::disconnectLDAP($ds);
+
+		$fid = fopen('/var/www/html/server/apps/ldapusermanagement/log.txt', 'w');
+		fwrite($fid, "deleteLDAPUser: " . $params['uid'] . " >> " . $params['password'] . " >> $result \n");
+		fclose($fid);
+	}
+
+
+	private static function connectLDAP() {
+		# code...
+		// LDAP variables
+		$ldaphost = "localhost";  // your ldap servers
+		$ldapport = 389;                 // your ldap server's port number
+
+		// Connecting to LDAP
+		$ds = $ldapconn = ldap_connect($ldaphost, $ldapport)
+		          or die("Could not connect to $ldaphost");
+
+		if ($ds) {
+		    // bind with appropriate dn to give update access
+		    // $r = ldap_bind($ds, "cn=admin, dc=localhost", "abb3h5Mv");
+
+			// $dn = "dc=localhost";
+			// $filter="(objectClass=inetOrgPerson)";
+			// $justthese = array("ou", "sn", "givenname", "mail");
+		 //    $sr=ldap_search($ds, $dn, $filter, $justthese);
+
+			// $info = ldap_get_entries($ds, $sr);
+		 //    ldap_close($ds);
+
+			// return $info["count"]." entries returned\n";
+			return $ds;
+
+		} else {
+		    return "Unable to connect to LDAP server";
+		}
+	}
+
+	private static function disconnectLDAP($ds) {
+		return ldap_unbind($ds);
+
+	}
+
 }
