cbd83762a88e19a210e3afa7c6310d3b33df564e
Côme Chilliet <come.chilliet@nextcloud.com>
2023-12-18 10:44:52 +0100

    Copy run.sh from notifications repo and adapt it
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -1,44 +1,17 @@
 #!/usr/bin/env bash
 
-OC_PATH=../../../../
-OCC=${OC_PATH}occ
-SCENARIO_TO_RUN=$1
-HIDE_OC_LOGS=$2
-
-# Nextcloud integration tests composer
-(
-    cd ${OC_PATH}build/integration
-    composer install
-)
-INSTALLED=$($OCC status | grep installed: | cut -d " " -f 5)
-
-if [ "$INSTALLED" == "true" ]; then
-    $OCC app:enable user_ldap
-    $OCC app:enable ldap_write_support
-else
-    echo "Nextcloud instance needs to be installed" >&2
-    exit 1
-fi
-
+APP_INTEGRATION_DIR=$PWD
+ROOT_DIR=${APP_INTEGRATION_DIR}/../../../..
 composer install
 composer dump-autoload
 
-# avoid port collision on jenkins - use $EXECUTOR_NUMBER
-if [ -z "$EXECUTOR_NUMBER" ]; then
-    EXECUTOR_NUMBER=0
-fi
-PORT=$((8080 + $EXECUTOR_NUMBER))
-echo $PORT
-php -S localhost:$PORT -t $OC_PATH &
-PHPPID=$!
-echo $PHPPID
+${ROOT_DIR}/occ app:enable user_ldap
+${ROOT_DIR}/occ app:enable ldap_write_support
+${ROOT_DIR}/occ app:list | grep user_ldap
+${ROOT_DIR}/occ app:list | grep ldap_write_support
 
-export TEST_SERVER_URL="http://localhost:$PORT/ocs/"
-
-vendor/bin/behat
+export TEST_SERVER_URL="http://localhost:8080/"
+${APP_INTEGRATION_DIR}/vendor/bin/behat --colors -f junit -f pretty $1 $2
 RESULT=$?
 
-kill $PHPPID
-
-echo "runsh: Exit code: $RESULT"
 exit $RESULT
