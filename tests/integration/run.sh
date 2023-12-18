1d992cce17a796b794e0505b67f9d9eb54b85bd8
Côme Chilliet <come.chilliet@nextcloud.com>
2023-12-18 11:10:09 +0100

    Add openldap service, run server integration composer install
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -2,15 +2,23 @@
 
 APP_INTEGRATION_DIR=$PWD
 ROOT_DIR=${APP_INTEGRATION_DIR}/../../../..
+
+# Nextcloud integration tests composer
+(
+    cd ${ROOT_DIR}/build/integration
+    composer install
+)
+
 composer install
 composer dump-autoload
 
+${ROOT_DIR}/occ status
 ${ROOT_DIR}/occ app:enable user_ldap
 ${ROOT_DIR}/occ app:enable ldap_write_support
-${ROOT_DIR}/occ app:list | grep user_ldap
-${ROOT_DIR}/occ app:list | grep ldap_write_support
+${ROOT_DIR}/occ app:list
 
-export TEST_SERVER_URL="http://localhost:8080/"
+export TEST_SERVER_URL="http://localhost:8080/ocs/"
+
 ${APP_INTEGRATION_DIR}/vendor/bin/behat --colors -f junit -f pretty $1 $2
 RESULT=$?
 
