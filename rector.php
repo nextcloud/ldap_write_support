c9dcfebed92b1505f4113cfeeb920aff8d6e45da
Côme Chilliet <come.chilliet@nextcloud.com>
2024-09-19 16:00:53 +0200

    feat: Add rector dev dependency and basic configuration
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

---
+++ rector.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/lib',
+        __DIR__ . '/templates',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withTypeCoverageLevel(0);
