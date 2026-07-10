8b339518bdbe9cb5aefca63d30d9b8f5fd5af7db
Peter Ringelmann <peter.ringelmann@nextcloud.com>
2026-07-10 14:18:13 +0200

    feat: migrate admin settings to Vue 3
    Signed-off-by: Peter Ringelmann <peter.ringelmann@nextcloud.com>

--- templates/settings-admin.php
+++ templates/settings-admin.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 ?>
-<div id="ldap-write-support-admin-settings">
+<div id="ldap-write-support-admin-settings-mount">
 </div>
